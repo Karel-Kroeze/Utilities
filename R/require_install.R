7fe3110d1bc2bf18d1bd8461358b717f87f55a80
Karel-Kroeze <k.a.kroeze@utwente.nl>
2015-11-11 12:33:18 +0100

    asd

--- R/require_install.R
+++ R/require_install.R
@@ -1,10 +1,16 @@
 #' Installs a required package if not available.
 #'
 #' @param ... string package name.
+#' @param github string representation of github archive to install the package from.
 #' @return none
+#' @importFrom devtools install_github
 #' @export
-Require <- function(...){
+Require <- function(..., github = NULL){
   if (!require(...)){
+    if (!is.null(github)){
+      Require("devtools")
+      install_github(github)
+    }
     install.packages(...)
     if (!require(...)) stop("Package not found.")
   }
