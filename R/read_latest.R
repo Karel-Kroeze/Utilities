fcdf263fa86540dfeb0c3326c17274de6acd9db7
FluffierThanThou <fluffy.l2032@gmail.com>
2016-08-25 09:32:23 +0200

    add read.latest, rename read.xlsx.all to read.all.xlsx

---
+++ R/read_latest.R
@@ -0,0 +1,43 @@
+#' Reads the latest file in a folder.
+#'
+#' @param filetype csv, csv2, json or excel. Defaults to json.
+#' @param folder path to folder
+#' @param pattern pattern to match files on. Note that by default files are filtered by file extensions correct for the type.
+#' @param ... further arguments passed to jsonlite::fromJSON, read.csv, openxlsx::read.xlsx, etc.
+#' @import magrittr
+#' @importFrom jsonlite fromJSON
+#' @importFrom openxlsx read.xlsx
+#' @importFrom utils read.csv read.csv2
+read.latest <- function( folder = NULL, filetype = "json", pattern = NULL, ... ){
+  if (!(filetype %in% c("json", "csv", "csv2", "excel" ) ) )
+    stop( "invalid filetype")
+  if (is.null(folder))
+    stop( "no path to folder provided" )
+  if (is.null(pattern)){
+    if( filetype == "json" )
+      pattern = ".json"
+    if( filetype == "csv" | filetype == "csv2" )
+      pattern = ".csv"
+    if ( filetype == "excel" )
+      pattern = ".xls"
+  }
+
+  # start your job!
+  files <- list.files( folder, pattern )
+  info <- files %>% file.info
+  if( (files %>% length ) <= 0 )
+    stop( "no files matching '", pattern, "' at path '", folder, "'" )
+
+  # get latest
+  latest <- files[which.max(info$mtime)]
+  cat( latest )
+
+  # read
+  invisible( switch( filetype,
+          "json" = fromJSON( latest, ... ),
+          "csv" = read.csv( latest, ... ),
+          "csv2" = read.csv2( latest, ... ),
+          "excel" = read.xlsx( latest, ... ) ) )
+
+}
+
--- R/require_install.R
+++ R/require_install.R
@@ -1,17 +1,25 @@
 #' Installs a required package if not available.
 #'
-#' @param ... string package name.
-#' @param github string representation of github archive to install the package from.
+#' @param pkg string or literal package name.
+#' @param github string representation of github archive to install the package from if not found in local library. Will attempt to install from CRAN if left blank.
 #' @return none
 #' @importFrom devtools install_github
+#' @importFrom utils install.packages
 #' @export
-Require <- function(..., github = NULL){
-  if (!require(...)){
+Require <- function( pkg, github = NULL ){
+  pkg_name <- as.character(substitute(pkg))
+
+  loaded <- function( pkg_name) {
+    tryCatch( require(pkg_name, quietly = TRUE, character.only = TRUE),
+              warning = function(e) FALSE,
+              error = function(e) FALSE) }
+
+  if (!loaded(pkg_name) ){
     if (!is.null(github)){
       Require("devtools")
       install_github(github)
     }
-    install.packages(..., repos = "http://cran.us.r-project.org")
-    if (!require(...)) stop("Package not found.")
+    install.packages(pkg_name, repos = "http://cran.us.r-project.org" )
+    if (!loaded(pkg_name)) stop("Package not found.")
   }
 }
