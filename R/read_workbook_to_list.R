80e223f5c2f7c67070fe2f7e2b94b74f55945a6e
FluffierThanThou <fluffy.l2032@gmail.com>
2016-08-25 09:32:23 +0200

    improved Require added read.xlsx.all

---
+++ R/read_workbook_to_list.R
@@ -0,0 +1,16 @@
+#' Reads an xlsx file into a list of data.frames, one for each sheet.
+#'
+#' @param file path to xlsx workbook
+#' @import openxlsx
+#' @export
+#' @return list of data.frame
+read.xlsx.all <- function( file = NULL ){
+  if (is.null(file)) stop( "file is required" )
+
+  # get the sheets from the file
+  sheets <- getSheetNames( file )
+  names(sheets) <- sheets
+
+  # get the data
+  lapply( sheets, function( sheet ) read.xlsx( file, sheet ) )
+}
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(Utilities)
-
-test_check("Utilities")
